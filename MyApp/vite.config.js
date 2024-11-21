54ff205a3d4537d4a475b5389699532d818c796f
RSarvjyoti <sarvjyoti64@gmail.com>
2024-11-21 16:28:27 +0530

    some changes

--- MyApp/vite.config.js
+++ MyApp/vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import compression from 'vite-plugin-compression';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:'/',
-  plugins: [react(), compression()],
+  plugins: [react()],
 })
